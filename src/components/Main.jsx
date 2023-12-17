6b5e59551c0ec7a2b34d9ce9820cfbbe608e70f8
Devraj326 <141002491+Devraj326@users.noreply.github.com>
2023-12-17 23:37:39 +0530

    Add files via upload

---
+++ src/components/Main.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react'
+import { useDispatch,useSelector } from 'react-redux'
+import {listAction, listSelector} from '../redux/reducer'
+import { fetchUser } from '../redux/reducer';
+
+const Main = () => {
+
+    const dispatch = useDispatch();
+
+    const list = useSelector(listSelector);
+
+    useEffect(() => {
+    dispatch(fetchUser());
+    }, []);
+
+  return (
+    <div>
+        {
+            list.map((item,index)=>{
+                return(
+                    <div>
+                        {item.name}<br/>
+                        {item.email}
+                        <br /><br />
+                        <hr />
+                    </div>
+                )
+            })
+        }     
+    </div>
+  )
+}
+export default Main;
